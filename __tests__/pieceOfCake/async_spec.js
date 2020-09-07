3df3da4616c9fa565b81c80de7e1213eec0722db
lpdft <772346730@qq.com>
2020-09-07 17:52:13 +0800

    [ruyue] feat:async_spec-should trigger failure using reject

--- __tests__/pieceOfCake/async_spec.js
+++ __tests__/pieceOfCake/async_spec.js
@@ -48,7 +48,7 @@ describe('for asynchronous', () => {
       .then(() => {
         // <--start
         // Please write down the correct value. You should write the final result directly.
-        const expected = undefined;
+        const expected = ['Failed! >_<'];
         // --end->
 
         expect(logs).toEqual(expected);
